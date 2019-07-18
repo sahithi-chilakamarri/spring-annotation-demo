ad25e0a8cfc97d7965db8547375b85bbea7864b4
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-18 22:39:33 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
 
     public static void main(String[] args) {
+        //Creating object for application context class using annotation class
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationClass.class);
+        //Creating object using getbean method
         Movie movie=context.getBean("movieBean",Movie.class);
         movie.display();
         context.close();
