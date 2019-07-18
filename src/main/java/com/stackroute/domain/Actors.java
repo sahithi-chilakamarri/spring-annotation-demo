c9b123db2cb561316e4ee40e3ec6869f6145f9b1
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-18 20:47:11 +0530

    Update Actors.java

--- src/main/java/com/stackroute/domain/Actors.java
+++ src/main/java/com/stackroute/domain/Actors.java
@@ -6,23 +6,26 @@ public class Actors {
     private String name;
     private String gender;
     private int age;
-
+    //Setting the value
     @Value("Sahithi")
     public void setName(String name) {
 
         this.name = name;
     }
+    //Setting the value
     @Value("Female")
     public void setGender(String gender) {
 
         this.gender = gender;
     }
+    //Setting the value
     @Value("23")
     public void setAge(int age)
     {
 
         this.age = age;
     }
+    //Display the values
     public void display()
     {
 
